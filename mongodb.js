708992276f0a37371b14493feebb8b2d7e04f333
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-17 14:42:06 -0300

    collection de tasks creada

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-const holagit
+
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
@@ -12,8 +12,50 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Fabián',
-        age: 25
+    // db.collection('users').insertOne({
+    //     name: 'Fabián',
+    //     age: 25
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log(' Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    //})
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gustavoql',
+    //         age: 25
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean my bedroom',
+            completed: false
+        }, {
+            description: 'Learn new programming lenguages',
+            completed: true
+        }, {
+            description: 'Talk to friends more',
+            completed: true
+        }
+
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
     })
 }) 
