0b35625a2adb41a97b420f64b3aaed299f376772
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-19 02:33:32 -0300

    Setup básico de express y nodemon

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('testing!')
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
