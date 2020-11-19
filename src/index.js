b994fe6e108dc2e0a4c743083618b9570c6680c5
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-19 02:55:07 -0300

    tasks endpoint básico implementado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,18 @@ app.post('/users', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
