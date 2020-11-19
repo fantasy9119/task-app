82217acd3a3df834f1ac6d77893a2e0824c5e329
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-19 02:37:00 -0300

    refactory mongoosejs, creación directorio models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,45 +7,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
 
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Password must not contain word "password" ')
-            }
-        }
-    }
-})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -59,24 +21,3 @@ const Task = mongoose.model('Task', {
     } 
 })
 
-const task = new Task({
-    description: 'Sleep early',
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error', error)
-})
-
-// const me = new User({
-//    name: '     Fabián     ',
-//    email: 'MYMAIL@CMPC.cl',
-//    password: 'pass12345'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
