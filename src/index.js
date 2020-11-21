a3640703b57351ff12ee3af8c4bb2baa27d33f7c
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-21 05:14:01 -0300

    Login y sign up generando json web tokens

--- src/index.js
+++ src/index.js
@@ -11,7 +11,20 @@ app.use(userRouter)
 app.use(taskRouter)
 
 
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+const jwt = require('jsonwebtoken')
+
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {expiresIn: '7 seconds'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+}
+
+myFunction()
