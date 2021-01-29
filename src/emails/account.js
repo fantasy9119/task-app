3848f6d2fde0b884157b9b6bf313e1e21efbcbc4
fantasy9119 <fabianmazzarellau@gmail.com>
2021-01-29 16:13:50 -0300

    ENV VARIABLES SETUP READY

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,7 @@
 const sgMail = require ('@sendgrid/mail')
-const sendgridAPIKey = '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'
 
-sgMail.setApiKey(sendgridAPIKey)
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
--- src/models/user.js
+++ src/models/user.js
@@ -78,7 +78,7 @@ userSchema.methods.toJSON = function () {
 //Generates json web token for login and signup
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user.id.toString()}, 'thisismynewcourse')
+    const token = jwt.sign({ _id: user.id.toString()}, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
